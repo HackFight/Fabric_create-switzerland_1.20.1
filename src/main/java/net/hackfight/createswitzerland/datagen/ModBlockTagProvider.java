c990dcfccb681b67475f2b3dc2ddbf2344a5fb6c
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-03-04 19:25:36 +0100

    Datagen allows block mining

--- src/main/java/net/hackfight/createswitzerland/datagen/ModBlockTagProvider.java
+++ src/main/java/net/hackfight/createswitzerland/datagen/ModBlockTagProvider.java
@@ -3,6 +3,7 @@ package net.hackfight.createswitzerland.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.hackfight.createswitzerland.block.ModBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.ItemTags;
@@ -22,5 +23,28 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 				.add(ModBlocks.HAZEL_WOOD)
 				.add(ModBlocks.STRIPPED_HAZEL_LOG)
 				.add(ModBlocks.STRIPPED_HAZEL_WOOD);
+
+		getOrCreateTagBuilder(BlockTags.AXE_MINEABLE)
+				.add(ModBlocks.HAZEL_LOG)
+				.add(ModBlocks.HAZEL_PLANKS);
+
+		getOrCreateTagBuilder(BlockTags.HOE_MINEABLE)
+				.add(ModBlocks.HAZEL_LEAVES);
+
+		getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+				.add(ModBlocks.CARAMEL_BLOCK)
+				.add(ModBlocks.CARAMEL_SLAB)
+				.add(ModBlocks.CARAMEL_STAIRS)
+				.add(ModBlocks.DARK_CHOCOLATE_BLOCK)
+				.add(ModBlocks.DARK_CHOCOLATE_STAIRS)
+				.add(ModBlocks.DARK_CHOCOLATE_SLAB)
+				.add(ModBlocks.MILK_CHOCOLATE_BLOCK)
+				.add(ModBlocks.MILK_CHOCOLATE_STAIRS)
+				.add(ModBlocks.MILK_CHOCOLATE_SLAB)
+				.add(ModBlocks.WHITE_CHOCOLATE_BLOCK)
+				.add(ModBlocks.WHITE_CHOCOLATE_STAIRS)
+				.add(ModBlocks.WHITE_CHOCOLATE_SLAB);
+
+
 	}
 }
